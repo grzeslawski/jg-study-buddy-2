7d716e209a4e9cd37ef07063a6a9fd9c50ee5ebe
Jarosław Grzesik <jaroslaw.grzesik94@gmail.com>
2021-12-05 19:49:18 +0100

    create users list component and mock data.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'index.css';
-import App from 'views/App.js';
+import Root from 'views/Root.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
