93a4b83657f1dee47fb0606df6f4f141d37bd648
Jarosław Grzesik <jaroslaw.grzesik94@gmail.com>
2021-12-12 21:08:15 +0100

    add react-context

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,63 +1,32 @@
 import { GlobalStyle } from 'assets/styles/GlobalStyle.js';
 import { theme } from 'assets/styles/theme.js';
 import { Wrapper } from './Root.styles.js';
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { users as usersData } from 'data/users';
 import MainTemplate from 'components/templates/MainTemplate.js';
 import AddUser from './AddUser.js';
 import Dashboard from './Dashboard.js';
-
-const initialFormState = {
-  name: '',
-  attendance: '',
-  average: '',
-};
+import UsersProvider from 'providers/UsersProvider.js';
 
 function Root() {
-  const [users, setUsers] = useState(usersData);
-  const [formValues, setFormValues] = useState(initialFormState);
-
-  const deleteUser = (name) => {
-    const filteredUsers = users.filter((user) => user.name !== name);
-    setUsers(filteredUsers);
-  };
-
-  const handleInputChange = (e) => {
-    setFormValues({
-      ...formValues,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleAddUser = (e) => {
-    e.preventDefault();
-    const newUser = {
-      name: formValues.name,
-      attendace: formValues.attendance,
-      average: formValues.average,
-    };
-
-    setUsers([newUser, ...users]);
-    setFormValues(initialFormState);
-  };
-
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <MainTemplate>
-          <Wrapper>
-            <Switch>
-              <Route path="/add-user">
-                <AddUser formValues={formValues} handleAddUser={handleAddUser} handleInputChange={handleInputChange} />
-              </Route>
-              <Route path="/">
-                <Dashboard deleteUser={deleteUser} users={users} />
-              </Route>
-            </Switch>
-          </Wrapper>
+          <UsersProvider>
+            <Wrapper>
+              <Switch>
+                <Route path="/add-user">
+                  <AddUser />
+                </Route>
+                <Route path="/">
+                  <Dashboard />
+                </Route>
+              </Switch>
+            </Wrapper>
+          </UsersProvider>
         </MainTemplate>
       </ThemeProvider>
     </Router>
