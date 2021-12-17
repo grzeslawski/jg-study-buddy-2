af40e7749377eac3f96c35e73e06f0132dee1a65
Jarosław Grzesik <jaroslaw.grzesik94@gmail.com>
2021-12-17 22:38:33 +0100

    add news section and search bar

--- src/views/Root.js
+++ src/views/Root.js
@@ -4,7 +4,7 @@ import { Wrapper } from './Root.styles.js';
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import MainTemplate from 'components/templates/MainTemplate.js';
+import MainTemplate from 'components/templates/MainTemplate/MainTemplate.js';
 import AddUser from './AddUser.js';
 import Dashboard from './Dashboard.js';
 import UsersProvider from 'providers/UsersProvider.js';
